e74271e6531a7d9bad3f624be3bf124942791b2a
Laeni <m@laeni.cn>
2018-12-13 19:04:06 +0800

    用户模块

--- user/src/main/java/cn/laeni/user/UserApplication.java
+++ user/src/main/java/cn/laeni/user/UserApplication.java
@@ -1,10 +1,12 @@
 package cn.laeni.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /*@ComponentScan
@@ -21,6 +23,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @EnableFeignClients        // 启动 Spring Cloud Feign 的支持
 @EnableRedisHttpSession    // 将SESSION内容存入Redis数据库中
 @ServletComponentScan        // 开启Servlet/Filter/Listener自动扫描
+@ImportResource
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
